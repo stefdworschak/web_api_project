0aea7b4e7b6a6613f05c49d366740a9747be653d
Stefan Dworschak <stef.dworschak@gmail.com>
2019-12-08 23:28:24 +0000

    added reference to module content

--- src/main/java/com/mycompany/jerseytutorial/services/CustomerService.java
+++ src/main/java/com/mycompany/jerseytutorial/services/CustomerService.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ **********************************************************************************************
+ *** The codebase comes originally from the MyBlogSolutionWithCommentsAndDBStubFile lab file***
+ ********************************************************************************************** 
  */
 package com.mycompany.jerseytutorial.services;
 
